a749e44b0b2ebb1aa4b164682bca0b34cc5bee28
Dawid Slowik <dawidslowik379@gmail.com>
2024-06-01 19:01:12 +0200

    Add hero section

---
+++ src/components/FlipWords.tsx
@@ -0,0 +1,68 @@
+"use client";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { AnimatePresence, motion, LayoutGroup } from "framer-motion";
+import { cn } from "../utils/cn";
+
+export const FlipWords = ({
+  word,
+  duration = 3000,
+  className,
+  delay = 0,
+  animationDuration = 0.08,
+}: {
+  word: string;
+  duration?: number;
+  className?: string;
+  delay?: number;
+  animationDuration?: number;
+}) => {
+  return (
+    <AnimatePresence>
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 10,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 0.4,
+          ease: "easeInOut",
+          type: "spring",
+          stiffness: 100,
+          damping: 10,
+        }}
+        exit={{
+          opacity: 0,
+          y: -40,
+          x: 40,
+          filter: "blur(8px)",
+          scale: 2,
+          position: "absolute",
+        }}
+        className={cn(
+          "z-10 inline-block relative text-left text-neutral-900 dark:text-neutral-100 px-2",
+          className
+        )}
+        key={word}
+      >
+        {word.split("").map((letter: string, index: number) => (
+          <motion.span
+            key={word + index}
+            initial={{ opacity: 0, y: 10, filter: "blur(8px)" }}
+            animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+            transition={{
+              delay: index * animationDuration,
+              duration: 0.4,
+            }}
+            className="inline-block"
+          >
+            {letter === " " ? "\u00a0" : letter}
+          </motion.span>
+        ))}
+      </motion.div>
+    </AnimatePresence>
+  );
+};
---
+++ src/components/Nav.tsx
@@ -0,0 +1,113 @@
+import instagram from "../../public/icons/instagram.svg";
+import logo from "../../public/logo.webp";
+import { motion } from "framer-motion";
+import { useState } from "react";
+import InstagramIcon from "../icons/InstagramIcon";
+import TiktokIcon from "../icons/TiktokIcon";
+
+const navMotion = {
+  visible: {
+    opacity: 1,
+
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.15,
+    },
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+const itemMotion = {
+  visible: { opacity: 1, x: 0 },
+  hidden: { opacity: 0, x: -100 },
+};
+const itemMotionDesktop = {
+  visible: { opacity: 1, x: 0 },
+  hidden: { opacity: 1, x: 0 },
+};
+const navLinks = [
+  { name: "O nas", href: "/o-nas", id: 1 },
+  { name: "Oferta", href: "/", id: 2 },
+  { name: "Kontakt", href: "/", id: 3 },
+];
+
+const NavLinks = ({
+  isMobile,
+  className,
+}: {
+  isMobile: boolean;
+  className: string;
+}) => (
+  <div className={className}>
+    {navLinks.map(({ name, href, id }) => (
+      <motion.a
+        key={id}
+        variants={isMobile ? itemMotion : itemMotionDesktop}
+        href={href}
+        className="text-center text-primary text-cormorant text-xl flex justify-center items-center whitespace-nowrap"
+      >
+        {name}
+      </motion.a>
+    ))}
+    <div className="flex gap-8">
+      <a href="https://www.instagram.com">
+        <InstagramIcon fill="#6c4e3f" />
+      </a>
+      <a href="https://www.tiktok.com">
+        <TiktokIcon fill="#6c4e3f" />
+      </a>
+    </div>
+  </div>
+);
+
+export default function Nav() {
+  const [toggled, setToggled] = useState(false);
+  return (
+    <nav className="relative px-4  flex items-center justify-between  font-medium  max-w-[1440px] mx-auto">
+      <div className="hidden xl:flex xl:items-center  xl:justify-center xl:gap-12 xl:text-lg  xl:ml-auto ">
+        <NavLinks className="flex gap-12" isMobile={false} />
+      </div>
+
+      {toggled && (
+        <motion.div
+          variants={navMotion}
+          animate="visible"
+          initial="hidden"
+          className="fixed left-0 top-0  z-40 flex h-screen
+          w-full flex-col items-center  justify-center  gap-24 bg-white text-2xl font-bold"
+        >
+          <NavLinks className="flex flex-col gap-24 text-lg " isMobile={true} />
+        </motion.div>
+      )}
+
+      {/* Hamburger Toggle */}
+      <motion.div
+        animate={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: 25 }}
+        transition={{ delay: 0.35 }}
+        onClick={() => setToggled((prevToggle) => !prevToggle)}
+        className={`burger z-50 cursor-pointer space-y-1.5 xl:hidden 
+        `}
+      >
+        <motion.span
+          animate={{ rotateZ: toggled ? 45 : 0, y: toggled ? 8 : 0 }}
+          className="line-1 bg-primary block h-0.5 w-8 bg-content"
+        ></motion.span>
+
+        <motion.span
+          animate={{ width: toggled ? 0 : 24 }}
+          className="line-2 bg-primary block h-0.5 w-6 bg-content"
+        ></motion.span>
+        <motion.span
+          animate={{
+            rotateZ: toggled ? -45 : 0,
+            y: toggled ? -8 : 0,
+            width: toggled ? 32 : 24,
+          }}
+          className="line-3 bg-primary block h-0.5 w-4 bg-content"
+        ></motion.span>
+      </motion.div>
+    </nav>
+  );
+}
