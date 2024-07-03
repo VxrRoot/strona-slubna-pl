7ccedbe1e164ba15522023d10543040e9760bbaa
Dawid Slowik <dawidslowik379@gmail.com>
2024-07-03 21:49:32 +0200

    Add realization example

---
+++ src/sections/realization/components/Nav.tsx
@@ -0,0 +1,126 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+import Logo from "./Logo";
+
+const navMotion = {
+  visible: {
+    opacity: 1,
+
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.15,
+    },
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
+const itemMotion = {
+  visible: { opacity: 1, x: 0 },
+  hidden: { opacity: 0, x: -100 },
+};
+const itemMotionDesktop = {
+  visible: { opacity: 1, x: 0 },
+  hidden: { opacity: 1, x: 0 },
+};
+const navLinks = [
+  { name: "Kiedy?", href: "/", id: 1 },
+  { name: "Jak dojechać?", href: "/", id: 2 },
+];
+const navLinks2 = [
+  { name: "Stoliki", href: "/", id: 2 },
+  { name: "Obecność", href: "/", id: 3 },
+];
+
+const NavLinks = ({
+  isMobile,
+  className,
+  items,
+}: {
+  isMobile: boolean;
+  className: string;
+  items: any[];
+}) => (
+  <div className={className}>
+    {items.map(({ name, href, id }) => (
+      <motion.a
+        key={id}
+        variants={isMobile ? itemMotion : itemMotionDesktop}
+        href={href}
+        className=" text-primary text-center flex justify-center items-center whitespace-nowrap w-20"
+      >
+        {name}
+      </motion.a>
+    ))}
+  </div>
+);
+
+export default function Nav() {
+  const [toggled, setToggled] = useState(false);
+  return (
+    <nav className="text-xl  mb-24 flex items-center absolute justify-between w-screen justify-center pb-6 pt-6 font-medium xl:grid grid-cols-3 bg-creme/50 backdrop-blur-md mx-auto text-red-50">
+      <div className="lg:hidden ml-8">
+        <Logo />
+      </div>
+      {/* Nav Items animating in  */}
+      {toggled && (
+        <motion.div
+          variants={navMotion}
+          animate="visible"
+          initial="hidden"
+          className="fixed left-0 top-0  z-40 flex h-screen
+          w-full flex-col items-center  justify-center  gap-24 bg-white text-2xl font-bold"
+        >
+          <NavLinks
+            className="flex flex-col gap-24 text-lg "
+            items={navLinks}
+            isMobile={true}
+          />
+          <NavLinks
+            className="flex flex-col gap-24 text-lg "
+            items={navLinks2}
+            isMobile={true}
+          />
+        </motion.div>
+      )}
+      <motion.div
+        animate={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: 10 }}
+        transition={{ delay: 0.35 }}
+        className="hidden xl:flex xl:items-center w-screen  xl:justify-center xl:gap-12 xl:ml-auto "
+      >
+        <NavLinks className="flex gap-12" items={navLinks} isMobile={false} />
+        <Logo />
+        <NavLinks className="flex gap-12" items={navLinks2} isMobile={false} />
+      </motion.div>
+
+      {/* Hamburger Toggle */}
+      <motion.div
+        animate={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: 25 }}
+        transition={{ delay: 0.35 }}
+        onClick={() => setToggled((prevToggle) => !prevToggle)}
+        className={`burger z-50 cursor-pointer space-y-1.5 xl:hidden mr-8
+        `}
+      >
+        <motion.span
+          animate={{ rotateZ: toggled ? 45 : 0, y: toggled ? 8 : 0 }}
+          className="line-1 bg-black block h-0.5 w-8 bg-content"
+        ></motion.span>
+
+        <motion.span
+          animate={{ width: toggled ? 0 : 24 }}
+          className="line-2 bg-black block h-0.5 w-6 bg-content"
+        ></motion.span>
+        <motion.span
+          animate={{
+            rotateZ: toggled ? -45 : 0,
+            y: toggled ? -8 : 0,
+            width: toggled ? 32 : 24,
+          }}
+          className="line-3 bg-black block h-0.5 w-4 bg-content"
+        ></motion.span>
+      </motion.div>
+    </nav>
+  );
+}
---
+++ src/sections/realization/components/Spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+    return (
+      <div className="flex justify-center items-center">
+        <div className="rounded-full h-6 w-6 border-4 border-b-creme animate-spin border-transparent"></div>
+      </div>
+    );
+  };
+  
+  export default Spinner;
