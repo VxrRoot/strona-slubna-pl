1b0f4b00f0bbd71ae549b1c05357193a310817f3
Dawid Slowik <dawidslowik379@gmail.com>
2024-07-15 23:23:03 +0200

    Add sitemap and privacy policy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
 import dotenv from "dotenv";
 
@@ -9,7 +10,12 @@ dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  site: "https://www.strona-slubna.pl",
+  integrations: [
+    react(),
+    tailwind(),
+    sitemap({ filter: (page) => !page.includes("/dziekujemy") }),
+  ],
   output: "hybrid",
   adapter: vercel(),
 });
